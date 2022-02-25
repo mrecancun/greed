1dc548f439a88fa07a8f847671d19cc2375166bc
Jelloritimo <83890853+Jelloritimo@users.noreply.github.com>
2022-02-25 22:26:03 +0900

    Update __main__.py
    Added explanatory comments.

--- greed/greed/__main__.py
+++ greed/greed/__main__.py
@@ -23,29 +23,30 @@ FONT_SIZE = 15
 COLS = 60
 ROWS = 40
 CAPTION = "Greed"
-# DATA_PATH = os.path.dirname(os.path.abspath(__file__)) + "/data/messages.txt"
 WHITE = Color(255, 255, 255)
 DEFAULT_ARTIFACTS = 40
+"""The two numbers '42' and '111' represent '*' and 'o' """
 twoArtifacts=[42,111]
 
 def main():
     
-    # create the cast
+    """Creates the cast"""
     cast = Cast()
     
-    # create the score_keeper
+    """Creates the score keeper"""
     score = Score()
-    score.set_text("SCORE: 0") # crear score score keeping. This line leaves the score text empty.
+    """This line leaves the score text empty."""
+    score.set_text("SCORE: 0")
     score.set_font_size(FONT_SIZE)
     score.set_color(WHITE)
     score.set_position(Point(CELL_SIZE, 0))
     cast.add_actor("scores", score)
     score_text=score._text
     
-    # create the robot
+    """Creates the robot"""
     x = int(MAX_X / 2)
     y = int(MAX_Y  -25)
-    position = Point(x, y) #ubicacion cambiar abajo de la pantalla
+    position = Point(x, y) 
 
     robot = Actor()
     robot.set_text("#")
@@ -53,16 +54,10 @@ def main():
     robot.set_color(WHITE)
     robot.set_position(position)
     cast.add_actor("robots", robot)
-    
-    # create the artifacts
-    # with open(DATA_PATH) as file:
-        # data = file.read()
-        # messages
 
     for n in range(DEFAULT_ARTIFACTS):
         ind=random.randint(0, 1)
         text = chr(twoArtifacts[ind])
-        # message = messages[n]
 
         x = random.randint(1, COLS - 1)
         y = random.randint(1, ROWS - 1)
@@ -75,17 +70,17 @@ def main():
         b = random.randint(0, 255)
         color = Color(r, g, b)
         
+        """Creates the Artifact"""
         artifact = Artifact()
         artifact.set_text(text)
         artifact.set_font_size(FONT_SIZE)
         artifact.set_color(color)
         artifact.set_position(position)
-        # artifact.set_message(score_text) Capaz se puede crear una funcion para añadir a score
         artifact.set_velocity(Point(0,5))
         cast.add_actor("artifacts", artifact)
         
     
-    # start the game
+    """Starts the game"""
     keyboard_service = KeyboardService(CELL_SIZE)
     video_service = VideoService(CAPTION, MAX_X, MAX_Y, CELL_SIZE, FRAME_RATE)
     director = Director(keyboard_service, video_service)
