e43dfdaf4377a668890a3317cdf4b849b0e432dd
Jelloritimo <83890853+Jelloritimo@users.noreply.github.com>
2022-02-25 22:32:45 +0900

    Update score.py
    Added explanatory comments.

--- greed/greed/game/casting/score.py
+++ greed/greed/game/casting/score.py
@@ -5,15 +5,20 @@ class Score (Actor):
     def __init__(self):
         """Constructs a new actor"""
         self._score = 0
+        """Inherits __init__ from the Actor class"""
         super (). __init__ ()
 
     def subtract (self):
+        """Substracts the score by 1"""
         self._score -= 1
 
     def add (self):
+        """Adds the score by 1"""
         self._score += 1
 
     def get_score (self):
+        """Returns the score as its current value"""
         return self._score 
 
         
+
